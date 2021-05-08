2853a3c23e1fef8e11ef3f6bf0c0a492f664b28e
Francisco Puma <francis_pancho12@hotmail.com>
2021-05-08 01:51:45 -0700

    general improvements

--- assets/js/mainscript.js
+++ assets/js/mainscript.js
@@ -38,40 +38,104 @@ var timer = document.querySelector("#timer");
 
 //functions
 
+    // clear page function 
+    var clearHtmlandStart = function () {
+        pagediv.innerHTML = ""
+        //insert timer ability
+        gettingStarted ();
+        };
+
     //get started and circle through questions
 
     var gettingStarted = function () {
 
         for (var i = 0; i < questions.length; i++) {
-        var questionTitle = document.createElement ("div");
-        questionTitle.id = "question";
-        questionTitle.innerHTML = "<h1 class='center'>" + questions[i].title + "</h1>";
-        pagediv.appendChild(questionTitle);   
-        }
+            //clear previous html
+            pagediv.innerHTML = ""
+            //adding content
+                //add title
+                var questionTitle = document.createElement ("div");
+                questionTitle.id = "question";
+                questionTitle.innerHTML = "<h1 class='center'>" + questions[i].title + "</h1>" 
+                // add ul
+                var questionUl = document.createElement ("ul");
+                //add li elements
+                var questionLi0 = document.createElement ("li");
+                questionLi0.id = "0";
+                questionLi0.textContent = questions[i].choices[0]
+                var questionLi1 = document.createElement ("li");
+                questionLi1.id = "1";
+                questionLi1.textContent = questions[i].choices[1]
+                var questionLi2 = document.createElement ("li");
+                questionLi2.id = "2";
+                questionLi2.textContent = questions[i].choices[2]
+                var questionLi3 = document.createElement ("li");
+                questionLi3.id = "3";
+                questionLi3.textContent = questions[i].choices[3]
+
+                
+                //add answer 
+                var questionAnswer = document.createElement ("div");
+                questionAnswer.id = "answer";
+                questionAnswer.innerHTML = ""
+            
+                //append results
+                pagediv.appendChild(questionTitle);  
+                questionTitle.appendChild(questionUl);
+                questionUl.appendChild(questionLi0);  
+                questionUl.appendChild(questionLi1);
+                questionUl.appendChild(questionLi2);
+                questionUl.appendChild(questionLi3);  
+                pagediv.appendChild(questionAnswer);
+                
+                debugger; // !!!!!!START HERE!!!!!!! 
+                //find a way to  select the correct option using the IDs and move on use switch to check answers posibly 
+                // find a way to display answer. If i = 0 display nothing  (use switch)
+                //find a way to run timer 
+                //develop store high scores
+                }
     };
 
     //compare answers
 
     //time's up 
 
-    // clear page function 
-    var clearHtml = function () {
-        pagediv.innerHTML = ""
-        gettingStarted ();
-        };
-
 //local storage of data
 
 
 //event listeners 
-start.addEventListener("click", clearHtml);
+start.addEventListener("click", clearHtmlandStart);
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
 //// tests draft 
 
-// get started function
+// get started function original
 // var questionTitle = document.createElement ("div");
         // questionTitle.id = "question";
-        // questionTitle.innerHTML = "<h1 class='center'> something</h1>"
+        // questionTitle.innerHTML = "<h1 class='center'>" + questions[idexQuestionArray].title + "</h1>";
         // pagediv.appendChild(questionTitle);
+  //  };
+
+// get started function with for 
+// var gettingStarted = function () {
+        
+//     for (var i = 0; i < questions.length; i++) {
+//     var questionTitle = document.createElement ("div");
+//     questionTitle.id = "question";
+//     questionTitle.innerHTML = "<h1 class='center'>" + questions[i].title + "</h1>";
+//     pagediv.appendChild(questionTitle);   
+//     }
+// };
