b9904e9c5ef7c839e17ef7a4375715fecd5880b1
Francisco Puma <francis_pancho12@hotmail.com>
2021-05-07 23:07:54 -0700

    setting up js file

---
+++ assets/js/mainscript.js
@@ -0,0 +1,17 @@
+// arrays with list of questions and answer
+
+// initial variables 
+
+//inital document selectors 
+
+// variables as the quiz progresses
+
+//functions
+    //get started and circle through questions
+
+    //compare answers
+
+    //time's up 
+
+//local storage of data
+
