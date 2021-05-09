3599d63f36bbb6dda9515fb17336524b63de766f
Francisco Puma <francis_pancho12@hotmail.com>
2021-05-08 20:47:21 -0700

    added button functionality

--- assets/js/highScore.js
+++ assets/js/highScore.js
@@ -1,3 +1,16 @@
+var questiondiv = document.querySelector("#question");
+var pagediv = document.querySelector("#page-content");
+var clear = document.querySelector("#btn-clear");
+var goback = document.querySelector("#btn-goback");
+var mainContent = document.querySelector ("#main-content")
 
+//clear local storage 
+clear.addEventListener("click", function () {
+    localStorage.clear();
+});
 
+//go back
 
+goback.addEventListener("click", function () {
+    window.location.replace("./index.html");
+});
